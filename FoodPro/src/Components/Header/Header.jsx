6bc0667f97cea7641e4ebb42d6514cefee7e4516
Mayank singh <mayank23378@gmail.com>
2024-06-18 14:25:59 +0530

    done with header section

--- FoodPro/src/Components/Header/Header.jsx
+++ FoodPro/src/Components/Header/Header.jsx
@@ -3,20 +3,26 @@ import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <nav className="bg-gray-500 text-white p-4">
-      <ul className="flex justify-evenly">
+    <nav className="bg-teal-500 font-bold flex text-xl text-white p-3">
+      <span className=" font-serif pl-12 text-2xl text-red-600">H</span>unger
+      <span className="font-serif text-2xl text-red-600">H</span>UB
+      <ul className="flex text-lg pl-14 gap-12">
         <li>
-          <Link to="/" className="hover:text-gray-300">Home</Link>
+          <Link to="/" className="hover:text-gray-300">
+            Home
+          </Link>
         </li>
         <li>
-         <Link to="Contact" className="hover:text-gray-300">Contact</Link>
+          <Link to="MyOrder" className="hover:text-gray-300">
+            MyOrder
+          </Link>
+        </li>
+        {/* <li>
+          <Link to="Detail" className="hover:text-red-700">detail</Link>
         </li>
         <li>
-          <Link to="Detail" className="hover:text-gray-300">detail</Link>
-        </li>
-        <li>
-          <Link to="Support" className="hover:text-gray-300">support</Link>
-        </li>
+          <Link to="Support" className="hover:text-red-700">support</Link>
+        </li> */}
       </ul>
     </nav>
   );
--- FoodPro/src/main.jsx
+++ FoodPro/src/main.jsx
@@ -1,27 +1,31 @@
 import React from "react";
- import ReactDOM from 'react-dom/client'
- import App from "./App";
- import "./index.css";
- import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
- import Home from "./Components/Home/Home";
- import Contact from "./Components/Contact/Contact";
- import Details from "./Components/Details/Details";
- import Support from "./Components/Support/Support";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import MyOrder from "./Components/MyOrder/MyOrder";
+import Details from "./Components/Details/Details";
+import Support from "./Components/Support/Support";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='' element={<Home />} />
-      <Route path="Contact" element={<Contact/>}/>
-      <Route path="Detail" element={<Details/>}/>
-      <Route path="Support" element={<Support/>}/>
-
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="MyOrder" element={<MyOrder />} />
+      <Route path="Detail" element={<Details />} />
+      <Route path="Support" element={<Support />} />
     </Route>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
