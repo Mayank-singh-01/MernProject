6d30a38fa7493877b9ffb4416065a4edafa29ad1
Mayank singh <mayank23378@gmail.com>
2024-06-21 21:23:49 +0530

    mongodb seatup

--- FoodPro/Backend/Db.jsx
+++ FoodPro/Backend/Db.jsx
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 const mongoUrl = "mongodb://localhost:27017/yourDatabaseName"; // Add your database name
 
-const mongoDB = () => {
-  mongoose
-    .connect(mongoUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("MongoDB connected");
-    })
-    .catch((err) => {
-      console.error("Error connecting to MongoDB:", err);
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoUrl, {
     });
+    console.log("MongoDB connected");
+    const db = mongoose.connection.db;
+    const fetchedData = await db.collection("HungerHUB").find({}).toArray();
+    console.log("Here's the data:", fetchedData);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
 };
 
 module.exports = mongoDB;
--- FoodPro/Backend/index.jsx
+++ FoodPro/Backend/index.jsx
@@ -4,15 +4,12 @@ const connectMongoDB = require("./Db.jsx");
 const app = express();
 const port = 5000;
 
-// Connect to MongoDB
 connectMongoDB();
 
-// Define a route
 app.get("/", (req, res) => {
   res.send("Hello, it's Mayank");
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`App started at port ${port}`);
 });
