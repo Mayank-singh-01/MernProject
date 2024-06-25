aea0a4e61f08e59ff6eaa7f08941d370f63bbe5c
Mayank singh <mayank23378@gmail.com>
2024-06-25 08:42:29 +0530

    done for the day

--- FoodPro/src/Components/Header/Header.jsx
+++ FoodPro/src/Components/Header/Header.jsx
@@ -3,53 +3,35 @@ import { Link, NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <nav
-      className="bg-teal-400 font-bold flex text-xl text-white z-50 p-4 fixed top-0 w-full"
-      aria-label="Main Navigation"
-    >
-      <div className="flex items-center">
-        <span className="font-serif pl-12 text-2xl text-red-600">H</span>
-        unger
-        <span className="font-serif text-2xl text-red-600">H</span>
-        UB
-      </div>
+    <nav className="bg-teal-400 font-bold flex text-xl text-white z-50 p-4 fixed top-0 w-full">
+      <span className="font-serif pl-12 text-2xl text-red-600">H</span>unger
+      <span className="font-serif text-2xl text-red-600">H</span>UB
       <ul className="flex text-lg pl-14 gap-12">
         <li>
-          <Link
-            to="/"
-            exact
-            className="hover:text-red-800"
-            activeClassName="text-red-800"
-          >
+          <NavLink to="/" exact="true" className="hover:text-red-800">
             Home
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
-            to="/MyOrder"
-            className="hover:text-red-800"
-            activeClassName="text-red-800"
-          >
+          <NavLink to="/MyOrder" className="hover:text-red-800">
             MyOrder
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to="/CreatUser"
             className="font-serif border-solid border-gray-300 hover:border-gray-400 border-2 rounded-xl p-1 hover:text-red-800"
-            activeClassName="text-red-800 border-red-800"
           >
             SignUp
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to="/LogIn"
             className="font-serif border-solid border-gray-300 hover:border-gray-400 border-2 rounded-xl p-1 hover:text-red-800"
-            activeClassName="text-red-800 border-red-800"
           >
             LogIn
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
--- FoodPro/src/Components/Screen/Lgin.jsx
+++ FoodPro/src/Components/Screen/Lgin.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../Screen/app.css";
 
 export default function Login() {
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5002/api/createuser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: credentials.email,
+          password: credentials.password,
+        }),
+      });
+
+      const json = await response.json();
+      alert(json.message);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again later.");
+    }
+  };
+
+  const onChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="flex background-image ">
       <form className="bg-teal-500 bg-opacity-70 mx-auto p-10 rounded-lg">
@@ -27,6 +60,12 @@ export default function Login() {
         >
           Submit
         </button>
+        <Link
+          to="/CreatUser"
+          className="font-serif font-bold px-4 ml-4 py-3 bg-red-800 active:text-gray-500 text-white rounded-lg"
+        >
+          New User
+        </Link>
       </form>
     </div>
   );
